d5b84ef7d8edd8181c37e14620ec26502eae5ceb
trendSetters01 <trendycrypto01@gmail.com>
2024-01-30 03:37:56 -0500

    refactor: added cors check

--- src/api/transferbrucelee.ts
+++ src/api/transferbrucelee.ts
@@ -9,12 +9,12 @@ type GetDepositTXResponse = { statusCode: number; txn: any };
 router.post<{}, GetDepositTXResponse>(
   "/transfer-bruce-lee",
   async (req: Request, res: Response) => {
-    // const origin = req.get("origin");
+    const origin = req.get("origin");
 
-    // if (origin !== "https://phantoms-ashy.vercel.app") {
-    //   console.log(origin);
-    //   return res.status(403).json();
-    // }
+    if (origin !== "https://phantoms-ashy.vercel.app") {
+      console.log(origin);
+      return res.status(403).json();
+    }
 
     const { to } = req.body;
 
